259e3792d3905e06445c191e703bf999e7cf71b4
OyuOyuk <oyuruby412@gmail.com>
2026-05-06 21:48:23 +0800

    setting it up

---
+++ src/components/parts/Header.jsx
@@ -0,0 +1,11 @@
+import BrandBook from '../../assets/BrandBook.svg'
+import './component_parts.css'
+
+
+function Header(){
+
+    return <div className="Header">
+        <img src={BrandBook} alt="" />
+    </div>
+}
+export default Header;
---
+++ src/screens/homepage/HomePage.jsx
@@ -0,0 +1,10 @@
+import "./HomePage.css"
+
+function HomePage(){
+    return <div className="HomePageBG">
+        <h1>Home Page</h1>
+        <p>3</p>
+    </div>
+}
+export default HomePage;
+
